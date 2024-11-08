2846165355673092b2c5d2885e81178c452f3bf1
JakubNovvak <jakubszymonnowak@gmail.com>
2024-11-08 08:46:10 +0100

    Implement test layout of the navbar

--- index.js
+++ index.js
@@ -3,16 +3,22 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+const path = require('path');
 app.use(express.static('public'));
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, './src/Views'));
 
-app.get('/', (rq, res) => {
-    res.send(
-        '<h2>WTMSApp</h2>' + 
-        '<br>'+
-        'Aplikacja do zarządzania czasem pracy'
-    )
+// app.get('/', (rq, res) => {
+//     res.send(
+//         '<h2>WTMSApp</h2>' + 
+//         '<br>'+
+//         'Aplikacja do zarządzania czasem pracy'
+//     )
+// })
+
+app.get('/', (req, res) => {
+    res.render('index', {title: 'Strona Główna', isAdmin: false});
 })
 
 app.listen(3000, () => {
